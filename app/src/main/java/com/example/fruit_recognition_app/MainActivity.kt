8d019f1c27d3bcbc8888e7df4e19c75164c656be
Julia Duffy <jtduffy7@gmail.com>
2025-07-27 21:53:38 -0600

    Basic UI set up

--- app/src/main/java/com/example/fruit_recognition_app/MainActivity.kt
+++ app/src/main/java/com/example/fruit_recognition_app/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.fruit_recognition_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var resultText: TextView
+    private lateinit var cameraButton: Button
+    private lateinit var galleryButton: Button
+
+    private val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        if (granted) {
+            //takePicturePreview.launch(null)
+        } else {
+            Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        imageView = findViewById(R.id.imageView)
+        resultText = findViewById(R.id.resultText)
+        cameraButton = findViewById(R.id.cameraButton)
+        galleryButton = findViewById(R.id.galleryButton)
+
     }
 }
